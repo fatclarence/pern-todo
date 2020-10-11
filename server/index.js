134970c57976e2cfe7664165cb4c0fbb64299fae
fatclarence <ckoh.clarence.96@gmail.com>
2020-10-11 21:19:12 +0800

    create basic restful apis for todo app

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.post("/todos", async(req, res) => {
 // Get all todo
 app.get("/todos", async(req, res) => {
     try {
-        const allTodos = await pool.query("SELECT * FROM todos");
+        const allTodos = await pool.query("SELECT * FROM todo");
         res.json(allTodos.rows);
     } catch(err) {
         console.error(err.message);
@@ -38,11 +38,54 @@ app.get("/todos", async(req, res) => {
 });
 
 // Get a todo
+app.get("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1",
+            [id]
+        );
 
-// Update a todo 
+        // Just grab the first item in the rows returned
+        res.json(todo.rows[0]);
+    } catch(err) {
+        console.error(err.message);
+    }
+});
+
+// Update a todo specified by its id
+app.put("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2",
+            [
+                description,
+                id
+            ]
+        );
+
+        res.json("Todo number " + id + " updated!");
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 // Delete a todo
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query(
+            "DELETE FROM todo WHERE todo_id = $1",
+            [id]
+        );
+        res.json("Todo " + id + " was deleted!");
+    } catch (err) {
+        console.error(err.message);
+    }
 
+});
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
