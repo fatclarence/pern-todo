2a1b463d8c6cf3135b62fd1aa0cb12c59e51eff8
fatclarence <ckoh.clarence.96@gmail.com>
2020-10-26 18:11:44 +0800

    update serve file name

--- database.sql
+++ database.sql
@@ -1,6 +1,14 @@
 -- CREATE DATABASE perntodo;
 
 CREATE TABLE todo (
-    todo_id SERIAL PRIMARY KEY, 
+    todo_id BIGSERIAL PRIMARY KEY, 
     description VARCHAR(255)
+);
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    UNIQUE (email)
 );
