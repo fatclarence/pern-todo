3c1ea258d4838f0d72bcbbed10544e66405836c2
fatclarence <ckoh.clarence.96@gmail.com>
2020-10-26 18:28:42 +0800

    edit connectionstring for toggle between local and production dev

--- db.js
+++ db.js
@@ -1,27 +1,22 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // Dev env
-const devConfig = {
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    port: process.env.PG_PORT
-}
+// const devConfig = {
+//     user: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     port: process.env.PG_PORT
+// }
 
 // // This mimicks the heroku string
-// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}
-// @${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-// Production env
-const proConfig = {
-    connectionString: process.env.DATABASE_URL
-}
-
-
-const pool = new Pool(
-    process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+const pool = new Pool({
+    connectionString: isProduction ? proConfig : devConfig
+});
 
 module.exports = pool;
