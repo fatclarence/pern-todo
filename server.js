285f1a1d0fd742628c50df569da0caa18d2d5c03
fatclarence <ckoh.clarence.96@gmail.com>
2020-10-27 18:00:56 +0800

    fix routes in server file

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
 
 // ROUTES 
 app.get(Object.values(frontEndRoutes), (req, res) => {
-    res.sendFile(path.join(__dirname, "build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 // API endpoints
