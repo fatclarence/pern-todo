f94cef56d49d9e8b9a7090bdf9de22e3abe0ecd3
fatclarence <ckoh.clarence.96@gmail.com>
2020-10-27 18:28:43 +0800

    remove front end routes

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ const pool = require("./server/db");
 const PORT = process.env.PORT || 5000;
 const path = require("path");
 const apiRouter = require("./server/apiRouter");
-const frontEndRoutes = require("./client/src/routes/Routes");
+// const frontEndRoutes = require("./client/src/routes/Routes");
 // Run the express library 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+// app.use(express.static(path.join(__dirname, "client/build")));
 if (process.env.NODE_ENV === "production") {
     // app.use(express.static) => allows us to serve static content that we specify in build folder
     app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 // ROUTES 
-app.get(Object.values(frontEndRoutes), (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
+// app.get(Object.values(frontEndRoutes), (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 
 // API endpoints
 app.use("/api", apiRouter);
